3a94db31ad96e556931f61c2daca28eb57245a47
Kado12 <gonzalosh01@gmail.com>
2025-08-05 00:34:19 -0500

    feat(database): add salon entries with detailed attributes

---
+++ database/Datos.sql
@@ -0,0 +1,535 @@
+-- Turno Mañana CU
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A11-M', 'A11', 40, 1, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A12-M', 'A12', 40, 1, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A13-M', 'A13', 40, 1, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A21-M', 'A21', 40, 1, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A22-M', 'A22', 40, 1, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A23-M', 'A23', 40, 1, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A31-M', 'A31', 40, 1, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A32-M', 'A32', 40, 1, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A33-M', 'A33', 40, 1, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('B11-M', 'B11', 40, 1, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('B12-M', 'B12', 40, 1, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('B13-M', 'B13', 40, 1, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('B14-M', 'B14', 40, 1, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('B21-M', 'B21', 40, 1, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('B22-M', 'B22', 40, 1, 1, 1);
+-- Turno Tarde CU
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A12-T', 'A12', 40, 1, 2, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A13-T', 'A13', 40, 1, 2, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A21-T', 'A21', 40, 1, 2, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A22-T', 'A22', 40, 1, 2, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A23-T', 'A23', 40, 1, 2, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A31-T', 'A31', 40, 1, 2, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A32-T', 'A32', 40, 1, 2, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('A33-T', 'A33', 40, 1, 2, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('B11-T', 'B11', 40, 1, 2, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('B12-T', 'B12', 40, 1, 2, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('B13-T', 'B13', 40, 1, 2, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('B14-T', 'B14', 40, 1, 2, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('B21-T', 'B21', 40, 1, 2, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('B22-T', 'B22', 40, 1, 2, 1);
+-- Turno Mañana LC
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C1-M', 'C1', 40, 2, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C2-M', 'C2', 40, 2, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C3-M', 'C3', 40, 2, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C4-M', 'C4', 40, 2, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C5-M', 'C5', 40, 2, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C6-M', 'C6', 40, 2, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C7-M', 'C7', 40, 2, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C8-M', 'C8', 40, 2, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C9-M', 'C9', 40, 2, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C10-M', 'C10', 40, 2, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C11-M', 'C11', 40, 2, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C12-M', 'C12', 40, 2, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C13-M', 'C13', 40, 2, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C14-M', 'C14', 40, 2, 1, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C15-M', 'C15', 40, 2, 1, 1);
+-- Turno Noche LC
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C1-N', 'C1', 40, 2, 3, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C2-N', 'C2', 40, 2, 3, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C3-N', 'C3', 40, 2, 3, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C4-N', 'C4', 40, 2, 3, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C5-N', 'C5', 40, 2, 3, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C6-N', 'C6', 40, 2, 3, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C7-N', 'C7', 40, 2, 3, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C8-N', 'C8', 40, 2, 3, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C9-N', 'C9', 40, 2, 3, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C10-N', 'C10', 40, 2, 3, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C11-N', 'C11', 40, 2, 3, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C12-N', 'C12', 40, 2, 3, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C13-N', 'C13', 40, 2, 3, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C14-N', 'C14', 40, 2, 3, 1);
+INSERT INTO salons (
+      code,
+      name,
+      capacity,
+      sede_id,
+      turn_id,
+      registration_process_id
+   )
+VALUES ('C15-N', 'C15', 40, 2, 3, 1);
